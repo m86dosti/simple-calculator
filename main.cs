c37955ef16eacce1cb7fb9fef3a0db3c1aa558e4
𝓜𝓸𝓱𝓪𝓶𝓶𝓪𝓭 <150838661+m86dosti@users.noreply.github.com>
2024-09-13 04:17:52 -0700

    Create main.cs

---
+++ main.cs
@@ -0,0 +1,39 @@
+using System;
+
+class SimpleCalculator
+{
+    static void Main()
+    {
+        Console.WriteLine("Enter first number:");
+        double num1 = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("Enter second number:");
+        double num2 = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("Enter an operator (+, -, *, /):");
+        char operation = Console.ReadLine()[0];
+
+        double result;
+
+        switch (operation)
+        {
+            case '+':
+                result = num1 + num2;
+                break;
+            case '-':
+                result = num1 - num2;
+                break;
+            case '*':
+                result = num1 * num2;
+                break;
+            case '/':
+                result = num1 / num2;
+                break;
+            default:
+                Console.WriteLine("Invalid operator.");
+                return;
+        }
+
+        Console.WriteLine("The result is: {result}");
+    }
+}
